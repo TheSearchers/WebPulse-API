c9704eca9742094f6e3d0ee0090b930491732614
MarahAlrefaai <marahalrefaai17@gmail.com>
2022-06-25 19:03:19 +0300

    chat update marah

--- server.js
+++ server.js
@@ -169,6 +169,8 @@ io.on("connection", (socket) => {
 app.get("/logout", (req, res) => {
   res.clearCookie("jwt").render("webpulse.ejs");
 });
+
+
 function start(port) {
   server.listen(port, () => {
     console.log(`running on port ${port}`);
